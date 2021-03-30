9366bf62f4a69d6622f9a668e214fb10abc7fe95
wangqixuan <wangqixuan@viewhigh.com>
2021-03-30 10:09:31 +0800

    rpc方式调用实现点对点发送文本消息

---
+++ pkg/util/util.go
@@ -0,0 +1,31 @@
+package util
+
+import (
+	"fmt"
+	"net"
+)
+
+// 获取服务器Ip
+func GetServerIp() (ip string) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("net.Interfaces failed, err:", err.Error())
+		return ""
+	}
+
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						return ipnet.IP.String()
+					}
+				}
+			}
+		}
+	}
+
+	return ""
+}
